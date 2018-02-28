82ad428ae48479cbfcdbd539e1dd79bdc90a71f1
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-28 14:35:54 +1100

    SYRBOPOULOS, Dimitrios: 2018-02-28 14:35:54

---
+++ user/trying to make magic 8 balllllllllllssssssssssssssss.py
@@ -0,0 +1,31 @@
+"""
+program: magic 8 ball
+Name: Dimitrios Syrbopoulos
+Date: 28/02/2018
+Desc: to give people not helpful answers to there stupid questions 
+"""
+
+
+from random import randint
+loo = (randint(1, 10))
+input ('')
+if loo == 1:
+     print ('maybe')
+if loo == 2:
+     print ('NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO')
+if loo == 3:
+     print ('if you think so')
+if loo == 4:
+     print ('go figure it out yourslef, what do i look like an answer machine?')
+if loo == 5:
+     print ('ask ularu')
+if loo == 6:
+     print ('YEEEEEEEEEEEEEIIIIIIIIIIIIIISSSSSSSSSSSSSSSS')
+if loo == 7:
+     print ('come my budah')
+if loo == 8:
+     print ('i know da wae')
+if loo == 9:
+     print ('....................................................simon sucks.....................................................................')
+if loo == 10:
+     print ('ask the carry')
