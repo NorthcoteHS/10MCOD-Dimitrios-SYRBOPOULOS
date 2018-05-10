9053e8deea1bfe80ea68a59c12a8243e280570de
Dimitrios Syrbopoulos <syr0001@nhs.vic.edu.au>
2018-05-10 12:51:31 +1000

    this is a new and improve version so pay attention to this one PLEASE

--- user/triviagame.py
+++ user/triviagame.py
@@ -9,14 +9,15 @@ def answers():
 response = input ('Hello')
 if response == ' Hello':
     print ('welcome to the trivia quiz')
+    question = ['How many legs do spiders have?', 'Whats the capital of China?', 'How big is simons nose?', 'What year was Australia federated?', 'what animal has the scientific name pinguinus impennis?']
+    answers = ['8', 'BeiJing', '17km', '1901', 'Great Auk']
+    for i, item in enumerate(question):
+        print(item)
+        input('answers:')
+        print(answers[i])
 else:
     print ('bye')
 
-question = ['How many legs do spiders have?', 'Whats the capital of China?', 'How big is simons nose?', 'What year was Australia federated?', 'what animal has the scientific name pinguinus impennis?']
-answers = ['8', 'BeiJing', '17km', '1901', 'Great Auk']
-for i, item in enumerate(question):
-    print(item)
-    input('answers:')
-    print(answers[i])
+
 
 
